6d2884488b0b0747e4e3ac6c27af42b4be08388b
willandru <willandru@hotmail.com>
2021-12-11 15:57:34 -0500

    Done Task Form form Front end, formulario creado y valores capturados se muestran por consola

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,8 +1,84 @@
+import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material'
+import {useState, useEffect} from 'react'
 
-export default function TaskForm()  {
-    return (
-        <div>
-            Task Form
-        </div>
+export default function TaskForm() {
+
+
+    const [task, setTask] = useState({
+        title: '',
+        description: ''
+    }) 
+    
+    
+
+
+
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        console.log(task)
+    }
+
+    const hanldeChange = (e) => {
+        setTask({...task, [e.target.name]: e.target.value});
+    }
+
+
+    return(
+    <Grid container direction="column" alignItems="center" justifyContent="center">
+        <Grid imtem xs={3} >
+            <Card
+            sx={{mt:5}}
+            style={{
+                backgroundColor: "#1e272e",
+                padding: "1rem"
+            }}
+            >
+                <Typography variant='5' textAlign='center' color='white'>
+                    Create Task
+                </Typography>
+                <CardContent>
+                    <form onSubmit={handleSubmit}>
+                        <TextField  
+                            variant='filled'
+                            label='Write ur title'
+                            sx={{
+                                display: 'block',
+                                margin: '.5rem 0'
+                            }}
+                            name="title"
+                            onChange={hanldeChange}
+                            inputProps={{style: {color: "white"}}}
+                            InputLabelProps={{style: {color: "white"}}}
+
+                        />
+                         <TextField  
+                            variant='filled'
+                            label='Write ur Description'
+                            multiline
+                            rows={4}
+                            sx={{
+                                display: 'block',
+                                margin: '.5rem 0'
+                            }}
+
+                            name="description"
+                            onChange={hanldeChange}
+
+                            inputProps={{style: {color: "white"}}}
+                            InputLabelProps={{style: {color: "white"}}}
+                        />
+                        <Button variant='contained' color='primary' type='submit' >
+                                Save
+                        </Button>
+
+
+                    </form>
+                </CardContent>
+            </Card>
+
+        </Grid>
+    </Grid>
     )
 }

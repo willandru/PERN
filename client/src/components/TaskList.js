c9e2a05b6e4426449e431f40ecff8c615b258886
willandru <willandru@hotmail.com>
2021-12-11 17:05:14 -0500

    Ya tenemos la interfaz y estamos listando las tareas en la pantalla

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,9 +1,71 @@
+import { useEffect, useState } from "react";
+import { Button, Card, CardContent, Typography} from '@mui/material'
+
 
 
 export default function TaskList() {
+
+    const [tasks, setTasks] = useState([])
+
+    const loadTasks = async () =>{
+        const response = await fetch('http://localhost:3000/tasks')
+        const data= await response.json()
+        setTasks(data)
+    }
+
+    useEffect(() => {
+        loadTasks()
+    }, [])
+
     return (
-        <div>
-            Task List
-        </div>
+        <>
+            <h1>Task List</h1>
+            {
+                tasks.map(task => (
+                    <Card style={
+                        {
+                            marginBottom: ".7rem",
+                            backgroundColor: "#1e272e"
+                        }
+                    }>
+                        <CardContent style={
+                            {
+                                display: "flex",
+                                justifyContent: "space-between",
+                            }
+                        }>
+                            <div>
+                            <Typography>
+                                {task.title}
+                            </Typography>
+                            <Typography>
+                                {task.description}
+                            </Typography>
+                             </div>
+                            
+                            <div style={
+                                {
+                                    color: 'black'
+                                }
+                            }>
+                            <Button  variant='contained' color='inherit' onClick={()=> console.log('edit')}>
+                                Edit
+                            </Button >
+                            <Button variant='contained' color='warning' onClick={()=> console.log('delete')}
+                                style={{
+                                    marginLeft: ".5rem"
+                                }}
+                            >
+                                Delete
+                            </Button>
+                            </div>
+
+                            
+                        </CardContent>
+                    </Card>
+                ))
+            }
+        </>
+
     )
 }

d34d9d68d5ce1ec40d40497745afd2afb8857ef7
willandru <willandru@hotmail.com>
2021-12-11 13:32:37 -0500

    Navigation :: DONE

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,27 @@
-import {Button} from '@mui/material'
-export default function TaskForm()  {
+import {AppBar, Box, Button, Container, Toolbar, Typography} from '@mui/material'
+import {Link, useNavigate} from 'react-router-dom'
+
+export default function NavBar()  {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Nav Bar
-            <Button variant='contained' color="secondary">Click Me</Button>
-        </div>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position='static' color='transparent'>
+                <Container>
+                    <Toolbar>
+                        <Typography variante='h6' sx={{flexGrow: 1}}>
+                            <Link to="/" style={{textDecoration:"none", color:"#eee"}}>
+                            PERN Stack 
+                            </Link>
+                        </Typography>
+                        <Button variant='contained' onClick={() => navigate("/tasks/new")} >
+                            New Task
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Box>
     )
 }
 
